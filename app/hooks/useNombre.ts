513cfb3f575f1f2db07a56b7d44ae55489220362
Ignacio Tejera <itejera01@gmail.com>
2025-03-27 18:35:46 -0300

    Eliminado de todas las soluciones con Autenticación por decisiones de tiempo

--- app/hooks/useNombre.ts
+++
@@ -1,9 +0,0 @@
-import { useUser } from '@clerk/clerk-expo';
-
-export const useUserIdAndNickname = () => {
-  const { user } = useUser();
-  return [
-    user.id,
-    user.primaryEmailAddress.emailAddress.split('@')[0],
-  ];
-}
--- stores/persistance/createClientPersister.ts
+++
@@ -1,8 +0,0 @@
-import * as SQLite from "expo-sqlite";
-import { createExpoSqlitePersister } from "tinybase/persisters/persister-expo-sqlite/with-schemas";
-import { MergeableStore, OptionalSchemas } from "tinybase/with-schemas";
-
-export const createClientPersister = <Schemas extends OptionalSchemas>
-  ( storeId: string, 
-    store: MergeableStore<Schemas>) =>
-    createExpoSqlitePersister(store, SQLite.openDatabaseSync(storeId + ".db"));
